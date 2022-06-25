962661bcaf8d964f51c8928f892c4ae10ce1edaa
GokulRaj12 <gokul799@gmail.com>
2022-06-25 08:20:52 +0530

    changed app.js and index.js

--- netflixlikeapp/src/App.js
+++ netflixlikeapp/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row';
 
 function App() {
   return (
     <div className="App">
+      <Row></Row>
     </div>
   );
 }
---
+++ netflixlikeapp/src/components/Row.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Row = () => {
+    return(
+        <div>
+            <h3>
+                Top sites!!
+            </h3>
+        </div>
+    );
+};
+
+export default Row;
